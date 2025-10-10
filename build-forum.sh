f91c1edbce6ac5c3fdf06e595bfdcd745eb29a33
mike <mjoecohen@gmail.com>
2025-10-10 18:28:09 +0200

    testnet

--- build-forum.sh
+++ build-forum.sh
@@ -13,10 +13,10 @@ source ~/.profile;
 #cd ~/hubgit/sip18forum/sip18-forum-api
 #git pull https://github.com/radicleart/sip18-forum-api.git daoless
 docker login;
-docker build --build-arg NODE_ENV=mainnet -t mijoco/sip18_forum_api:latest .
+docker build --build-arg NODE_ENV=testnet -t mijoco/sip18_forum_api:latest .
 docker push mijoco/sip18_forum_api:latest
 docker rm -f sip18_forum_api
-docker run -d -t -i --network host -e NODE_ENV=mainnet --env-file ~/.env --name $DOCKER_NAME -p 6090:6090 mijoco/sip18_forum_api:latest
+docker run -d -t -i --network host -e NODE_ENV=testnet --env-file ~/.env --name $DOCKER_NAME -p 6090:6090 mijoco/sip18_forum_api:latest
 docker logs -f $DOCKER_NAME
 
 printf "Finished....\n"
